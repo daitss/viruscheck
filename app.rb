f4aa5a8f5cdac1f870da060ab97f266438f5f9d0
Francesco Lazzarino <flazzarino@gmail.com>
2010-08-16 16:46:53 -0400

    clamd :: Bool is an option now

--- app.rb
+++ app.rb
@@ -5,37 +5,55 @@ require 'bundler'
 Bundler.setup
 
 require 'sinatra'
+
 require 'uuid'
+require 'fileutils'
 
 # TODO have index form for individual use
-# TODO support clamdscan
 # TODO investigate http://github.com/eagleas/clamav
 
-configure do
-  output = %x{clamscan --version 2>&1}
-  raise "clamscan not found" unless output.lines.first =~ /ClamAV/
+helpers do
+
+  def clambin
+    settings.clamd ? 'clamdscan' : 'clamscan'
+  end
+
+  def clamscan data
+
+    # write to a tempfile
+    tf = Tempfile.new 'vc'
+    tf.write params['data']
+    tf.flush
+
+    # needed for clamav to read the file as a different user
+    FileUtils.chmod 0644, tf.path
+
+    # virus scan it
+    @output = `#{clambin} #{tf.path} 2>&1`
+    tf.close!
+
+    # check its status
+    @infected = case $?.exitstatus
+                when 0 then false
+                when 1 then true
+                else raise <<-MESSAGE
+problem with #{clambin} (#{$?.exitstatus}):
+                  #{@output}
+                  MESSAGE
+                end
+  end
+
+end
+
+
+configure do |s|
+  disable :clamd
 end
 
 post '/' do
-
   # check the parameters
   error 400, "Missing Data" unless params['data']
-
-  # write to a tempfile
-  tf = Tempfile.new 'vc'
-  tf.write params['data']
-  tf.flush
-
-  # virus scan it
-  @output = %x{clamscan #{tf.path} 2>&1}
-  tf.close!
-
-  # check its status
-  @infected = case $?.exitstatus
-              when 0 then false
-              when 1 then true
-              else raise "problem with virus checker: #{$?.exitstatus}"
-              end
+  clamscan params['data']
 
   # make an event id
   @event_id = UUID.generate :urn
--- config.ru
+++ config.ru
@@ -5,6 +5,9 @@ Bundler.setup
 require 'app'
 
 set :env, :production
+
+enable :clamd
+
 disable :run, :reload
 
 run Sinatra::Application
